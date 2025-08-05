0eeb35d539cd04165c22900169489927fd3e0b87
m-diogenes <d1ogenes.snw@gmail.com>
2025-08-05 14:11:42 +0000

    Implementando recursividade e loops aninhados

--- xadrez.c
+++ xadrez.c
@@ -1,49 +1,67 @@
 #include <stdio.h>
 
+// Torre
+// Mover torre 5 casas para a direita
+void recursivo(int numero) {
+    if (numero > 0) {
+        printf("Direita \n");   // Imprime a direção
+        recursivo(numero - 1);
+    }
+}
+
+// Rainha                  
+// Mover rainha 8 casas para a esquerda
+void recursivo2(int numero) {
+    if (numero > 0) {
+        printf("Esquerda\n");   // Imprime a direção
+        recursivo2(numero - 1);
+    }
+}
+
+// Bispo
+// Mover bispo 5 casa na diagonal
+void recursivo3(int numero) {
+    int i, j;
+    if (numero > 0) {
+        for (i = 1; i <= 1; i++) {
+            printf("Cima, ");   // Imprime a direção
+
+            for (j = 1; j <= 1; j++) {
+                printf("Direita\n");    // Imprime a direção
+            }
+        }
+        recursivo3(numero - 1);
+    }
+}
+
 int main() {
-    
-    int bispo = 0, rainha = 0, cavalo = 1;
 
-    //Torre
-    // Mover torre 5 casas para a direita
-    for (int torre = 0; torre < 5; torre ++) { 
-        printf("Direita\n");    //Imprime a direção do movimento
+    int torre = 5, bispo = 5, rainha = 8;   // Quantidade de movimentos
+    int i, j;
+
+    // Torre
+    recursivo(torre);   //Repetição de movimentos
+
+    printf("\n");   //Linha em branco
+
+    // Bispo
+    recursivo3(bispo);  //Repetição de movimentos
+
+    printf("\n");   //Linha em branco
+
+    // Rainha                  
+    recursivo2(rainha); //Repetição de movimentos
+
+    printf("\n");   //Linha em branco
+
+    // Cavalo
+    for (i = 1; i <= 2; i++) {
+        printf("Cima\n");       //Dois movimentos para cima
     }
 
-    printf("\n");   //Linha em branco
-
-    //Bispo
-    //Mover bispo 5 casa na diagonal
-    while (bispo < 5) {
-
-        printf("Cima, Direita\n");    //Imprime a direção do movimento
-
-        bispo ++;
-    }
-
-    printf("\n");   //Linha em branco
-
-    //Rainha
-    //Mover rainha 8 casas para a esquerda
-    do {
-
-        printf("Esquerda\n");
-        rainha++;   //Imprime a direção do movimento
-
-        }
-    while (rainha < 8);
-
-    printf("\n");   //Linha em branco
-
-    //Cavalo
-    //Mover cavalo 3 casas: 2 para baixo e 1 para a esquerda
-    while (cavalo--)
-    {
-        for (int i = 0; i < 2; i++)
-        {
-            printf("Baixo\n");      //Imprime a direção do movimento
-        }
-        printf("Esquerda\n");       //Imprime a direção do movimento
+    for (j = 1; j <= 2; j++) {
+        if (j > 1) break;
+        printf("Direita\n");    //Um movimento para direita
     }
 
     return 0;
